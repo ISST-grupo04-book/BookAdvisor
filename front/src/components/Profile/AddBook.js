6610a83e2147c8ad69271434d11800e90738cacb
GABRIELA <g.gcruz@alumnos.upm.es>
2020-04-29 08:52:48 +0200

    cambios addBook

--- front/src/components/Profile/AddBook.js
+++ front/src/components/Profile/AddBook.js
@@ -4,6 +4,82 @@ import '../../css/pseudoElement.css';
 import '../../css/Profile/AddBooks.css'
  
 export default class AddBook extends React.Component{
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            titulo : "",
+            ISBN : "",
+            editorial: "",
+            edicion: "",
+            autor: "",
+            categoria: "",
+            formato: "",
+            foto: "",
+            descripcion: ""
+        };
+        this.handleTitulo = this.handleTitulo.bind(this);
+        this.handleISBN = this.handleISBN.bind(this);
+        this.handleEditorial = this.handleEditorial.bind(this);
+        this.handleEdicion = this.handleEdicion.bind(this);
+        this.handleAutor = this.handleAutor.bind(this);
+        this.handleCategoria = this.handleCategoria.bind(this);
+        this.handleFormato = this.handleFormato.bind(this);
+        this.handleFoto = this.handleFoto.bind(this);
+        this.handleDescripcion = this.handleDescripcion.bind(this);
+        this.handleSubmit = this.handelSubmit.bind(this);
+    }
+
+
+    //Métodos para capturar los cambios de evento de cada input:
+    handleTitulo(e1){
+        this.setState({titulo : e1.curretTarget.value});
+    }
+    handleISBN(e2){
+        this.setState({ISBN : e2.curretTarget.value});
+    }
+    handleEditorial(e3){
+        this.setState({editorial : e3.curretTarget.value});
+    }
+    handleEdicion(e4){
+        this.setState({edicion : e4.curretTarget.value});
+    }
+    handleAutor(e5){
+        this.setState({autor : e5.curretTarget.value});
+    }
+    handleCategoria(e6){
+        this.setState({categoria : e6.curretTarget.value});
+    }
+    handleFormato(e7){
+        this.setState({formato : e7.curretTarget.value});
+    }
+    handleFoto(e8){
+        this.setState({foto : e8.curretTarget.value});
+    }
+    handleDescripcion(e9){
+        this.setState({descripcion : e9.curretTarget.value});
+    }
+
+    //Método submit
+    handleSubmit(e){
+        e.preventDefault();
+        if(titulo.trim() == "" || ISBN.trim() == "" || editorial.trim() == "" || edicion.trim() == "" || autor.trim() == "" || categoria.trim() == "" || formato.trim() == "" || foto == "" || descripcion == ""){
+            alert("Debe rellenar todos los campos para completar la información sobre el libro");
+        }else{
+            let paramsToUpdate = {
+                titulo : titulo.trim(),
+                ISBN : ISBN.trim(),
+                editorial: editorial.trim(),
+                edicion: edicion.trim(),
+                autor: edicion.trim(),
+                categoria: categoria.trim(),
+                formato: formato.trim(),
+                foto: foto.trim(),
+                descripcion: descripcion.trim()
+            }
+        }
+    }
+
     render(){
 
         const styles = {
@@ -43,11 +119,11 @@ export default class AddBook extends React.Component{
                 <Form.Row>
                     <Col>
                         <p style={styles.text}>Título</p>
-                        <Form.Control size="lg" type="text" style={styles.input}/>
+                        <Form.Control size="lg" type="text" style={styles.input} onChange={this.handleTitulo(e1)}/>
                     </Col>
                     <Col>
                         <p style={styles.text}>ISBN</p>
-                        <Form.Control size="lg" type="text" style={styles.input}/>
+                        <Form.Control size="lg" type="text" style={styles.input} onChange={this.handleISBN(e2)}/>
                     </Col>
                 </Form.Row>
 
@@ -55,11 +131,11 @@ export default class AddBook extends React.Component{
                 <Form.Row>
                     <Col>
                         <p style={styles.text}>Editorial</p>
-                        <Form.Control size="lg" type="text" style={styles.input}/>
+                        <Form.Control size="lg" type="text" style={styles.input} onChange={this.handleEditorial(e3)}/>
                     </Col>
                     <Col>
                         <p style={styles.text}>Edición</p>
-                        <Form.Control size="lg" type="text" style={styles.input}/>
+                        <Form.Control size="lg" type="text" style={styles.input} onChange={this.handleEdicion(e4)}/>
                     </Col>
                 </Form.Row>
 
@@ -67,11 +143,11 @@ export default class AddBook extends React.Component{
                 <Form.Row>
                     <Col>
                         <p style={styles.text}>Autor</p>
-                        <Form.Control size="lg" type="text" style={styles.input}/>
+                        <Form.Control size="lg" type="text" style={styles.input} onChange={this.handleAutor(e5)}/>
                     </Col>
                     <Col>
                         <p style={styles.text}>Categoría</p>
-                        <Form.Control as="select" placeholder="Seleccionar" size="lg" style={styles.input}>
+                        <Form.Control as="select" placeholder="Seleccionar" size="lg" style={styles.input} onChange={this.handleCategoria(e6)}>
                             <option>Narrativa</option>
                             <option>No ficción</option>
                             <option>Poesía</option>
@@ -88,7 +164,7 @@ export default class AddBook extends React.Component{
                 <Form.Row>
                     <Col>
                         <p style={styles.text}>Formato</p>
-                        <Form.Control size="lg" type="text" style={styles.input}/>
+                        <Form.Control size="lg" type="text" style={styles.input} onChange={this.handleFormato(e7)}/>
                     </Col>
 
                     <Col>
@@ -96,7 +172,7 @@ export default class AddBook extends React.Component{
                      <InputGroup className="picture">
                         <Form.Control size="lg" type="text" style={styles.input}/>
                             <InputGroup.Append>
-                                <Button id="addpicture" variant="outline-secondary" style={styles.link}>Añadir foto</Button>
+                                <Button id="addpicture" variant="outline-secondary" style={styles.link} onChange={this.handleFoto(e8)}>Añadir foto</Button>
                             </InputGroup.Append>
                     </InputGroup>
                     </Col>
@@ -106,12 +182,12 @@ export default class AddBook extends React.Component{
                 <Form.Row>
                     <Col>
                         <p style={styles.text}>Descripción</p>
-                        <Form.Control size="lg" type="text" style={{height:'150px'}}/>
+                        <Form.Control size="lg" type="text" style={{height:'150px'}} onChange={this.handleDescripcion(e9)}/>
                     </Col>
                 </Form.Row>
 
                 <span/>
-                <Button type="submit" id="submit"  style={styles.button}><p style={styles.text}>Enviar libro a revisión</p></Button>
+                <Button type="submit" id="submit"  style={styles.button} onClick={this.handleSubmit(e)}><p style={styles.text}>Enviar libro a revisión</p></Button>
             </Form.Group >
 
             </div>
